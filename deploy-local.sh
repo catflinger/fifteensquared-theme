04b2c6cfcff79a3430fea54a2e87dd35459a17eb
Paul Drury <paul@drurys.org>
2017-11-17 11:16:48 +0000

    more fixes to shell script

--- deploy-local.sh
+++ deploy-local.sh
@@ -3,5 +3,5 @@
 
 echo delpoying...
 
-rm -R /var/www/html/wp-content/themes/wp-andreas225
+[ -d /var/www/html/wp-content/themes/wp-andreas225] && rm -R /var/www/html/wp-content/themes/wp-andreas225
 mv ./wp-andreas225 /fifteensquared-theme$ ls /var/www/html/wp-content/themes
