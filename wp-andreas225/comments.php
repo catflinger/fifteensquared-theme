060a2a9c987fb965ac857d8adeff957f45bd6e9d
Paul Drury <paul@drurys.org>
2017-12-10 16:05:01 +0000

    Added missing comment count

--- wp-andreas225/comments.php
+++ wp-andreas225/comments.php
@@ -1,6 +1,13 @@
-<div id="comments">
+<div>
+
+    <h3 id="comments">
+        <?php comments_number('No Responses', 'One Response', '% Responses'); ?> 
+        to &#8220;<?php the_title(); ?>&#8221;
+    </h3>
+
     <ol class="commentlist">
         <?php wp_list_comments(); ?>
     </ol>
     <?php comment_form(); ?>
+    
 </div>
